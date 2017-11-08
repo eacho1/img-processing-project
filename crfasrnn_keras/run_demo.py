852341e3aaf650aa47faa157cb2c39d12d76bd93
eexie <eexie@users.noreply.github.com>
2017-11-07 20:15:23 -0500

    more testing images, check output/ and crfasrnn/images/

--- crfasrnn_keras/run_demo.py
+++ crfasrnn_keras/run_demo.py
@@ -27,6 +27,8 @@ import cv2
 import numpy as np
 import scipy
 import scipy.ndimage, scipy.ndimage.filters
+import argparse
+import glob
 from matplotlib import pyplot
 from skimage.exposure import rescale_intensity
 from PIL import Image
@@ -63,81 +65,89 @@ def convolve(image, list_points, kernel):
     return output
 
 def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--images", required=True,
+        help="path to input dataset of images")
+    args = vars(ap.parse_args())
     output_file = "labels.png"
-    image = cv2.imread("image.jpg")
-    scale = 500.0/max(image.shape[:2])
-    image = cv2.resize(image, (0,0), fx=scale, fy=scale)
-    cv2.imwrite("test.jpg", image)
-    input_file = "test.jpg"
-    # # Download the model from https://goo.gl/ciEYZi
-    saved_model_path = "crfrnn_keras_model.h5"
+    for imagePath in glob.glob(args["images"] + "/*"):
+        print("next")
+        # load the image
+        image = cv2.imread(imagePath)
+        scale = 500.0/max(image.shape[:2])
+        image = cv2.resize(image, (0,0), fx=scale, fy=scale)
+        cv2.imwrite("test.jpg", image)
+        input_file = "test.jpg"
+        # # Download the model from https://goo.gl/ciEYZi
+        saved_model_path = "crfrnn_keras_model.h5"
 
-    model = get_crfrnn_model_def()
-    model.load_weights(saved_model_path)
+        model = get_crfrnn_model_def()
+        model.load_weights(saved_model_path)
 
-    img_data, img_h, img_w = util.get_preprocessed_image(input_file)
-    probs = model.predict(img_data, verbose=False)[0, :, :, :]
-    label_mask, segmentation = util.get_label_image(probs, img_h, img_w) 
-    segmentation.save(output_file)
-    pyplot.imshow(label_mask)
-    pyplot.title("Label Mask")
-    pyplot.figure();
-    print label_mask
-    image_array_rgb = np.array(Image.open(input_file))
-    smallBlur = np.ones((21,21), dtype="float")*(1.0/(21*21))
-    r_original,g_original,b_original = np.split(image_array_rgb, 3, axis=2)
-    a_original = np.ones_like(r_original)*255
+        img_data, img_h, img_w = util.get_preprocessed_image(input_file)
+        probs = model.predict(img_data, verbose=False)[0, :, :, :]
+        label_mask, segmentation = util.get_label_image(probs, img_h, img_w) 
+        segmentation.save(output_file)
+        pyplot.imshow(label_mask)
+        pyplot.title("Label Mask")
+        pyplot.figure();
+        print label_mask
+        image_array_rgb = np.array(Image.open(input_file))
+        smallBlur = np.ones((21,21), dtype="float")*(1.0/(21*21))
+        r_original,g_original,b_original = np.split(image_array_rgb, 3, axis=2)
+        a_original = np.ones_like(r_original)*255
 
-    rgba_original = np.concatenate((r_original,g_original,b_original), axis=2).copy()
+        rgba_original = np.concatenate((r_original,g_original,b_original), axis=2).copy()
 
-    r    = r_original[1350:1650,2300:2600,0].copy().astype(np.uint8)
-    g    = g_original[1350:1650,2300:2600,0].copy().astype(np.uint8)
-    b    = b_original[1350:1650,2300:2600,0].copy().astype(np.uint8)
-    a    = np.ones_like(r) * 255
-    rgba = np.dstack((r,g,b,a)).copy()
-    pyplot.imshow(rgba_original)
-    pyplot.title("Original")
-    pyplot.figure()
+        r    = r_original[1350:1650,2300:2600,0].copy().astype(np.uint8)
+        g    = g_original[1350:1650,2300:2600,0].copy().astype(np.uint8)
+        b    = b_original[1350:1650,2300:2600,0].copy().astype(np.uint8)
+        a    = np.ones_like(r) * 255
+        rgba = np.dstack((r,g,b,a)).copy()
+        pyplot.imshow(rgba_original)
+        pyplot.title("Original")
+        pyplot.figure()
 
-    blur_kernel_width = np.int32(16)
-    blur_kernel_half_width = np.int32(8)
-    blur_sigma = np.float32(16)
+        blur_kernel_width = np.int32(16)
+        blur_kernel_half_width = np.int32(8)
+        blur_sigma = np.float32(16)
 
-    y,x = \
-        scipy.mgrid[-blur_kernel_half_width:blur_kernel_half_width+1,
-                -blur_kernel_half_width:blur_kernel_half_width+1]
-    blur_kernel_not_normalized = np.exp((-(x**2 + y**2))/(2 * blur_sigma**2))
-    normalization_constant = np.float32(1) / np.sum(blur_kernel_not_normalized)
-    blur_kernel = (normalization_constant * blur_kernel_not_normalized).astype(np.float32)
+        y,x = \
+            scipy.mgrid[-blur_kernel_half_width:blur_kernel_half_width+1,
+                    -blur_kernel_half_width:blur_kernel_half_width+1]
+        blur_kernel_not_normalized = np.exp((-(x**2 + y**2))/(2 * blur_sigma**2))
+        normalization_constant = np.float32(1) / np.sum(blur_kernel_not_normalized)
+        #blur_kernel = (normalization_constant * blur_kernel_not_normalized).astype(np.float32)
+        blur_kernel=smallBlur
 
-    pyplot.imshow(blur_kernel, cmap="gray", interpolation="nearest")
-    pyplot.title("blur_kernel")
-    pyplot.figure()
+        pyplot.imshow(blur_kernel, cmap="gray", interpolation="nearest")
+        pyplot.title("blur_kernel")
+        pyplot.figure()
 
-    convolution_filter = blur_kernel
+        convolution_filter = blur_kernel
 
-    r_blurred= scipy.ndimage.filters.convolve(r, convolution_filter, mode="nearest")
-    g_blurred= scipy.ndimage.filters.convolve(g, convolution_filter, mode="nearest")
-    b_blurred= scipy.ndimage.filters.convolve(b, convolution_filter, mode="nearest")
-    rgba_blurred = np.dstack((r_blurred, g_blurred, b_blurred))
+        r_blurred= scipy.ndimage.filters.convolve(r, convolution_filter, mode="nearest")
+        g_blurred= scipy.ndimage.filters.convolve(g, convolution_filter, mode="nearest")
+        b_blurred= scipy.ndimage.filters.convolve(b, convolution_filter, mode="nearest")
+        rgba_blurred = np.dstack((r_blurred, g_blurred, b_blurred))
 
-    #label_mask = np.ones((r_original.shape[0]-100,r_original.shape[1]-100))
-    label_mask = np.divide(label_mask, 15)
-    #print label_mask
-    list_mask = zip(np.where(label_mask<1)[1], np.where(label_mask<1)[0])
-    #pyplot.subplot(122),pyplot.imshow(label_mask, cmap="gray", interpolation="nearest")
-    #pyplot.title("label_mask");
-    
-    r_convolved = convolve(r_original, list_mask, convolution_filter)
-    g_convolved = convolve(g_original, list_mask, convolution_filter)
-    b_convolved = convolve(b_original, list_mask, convolution_filter)
-    rgba_convolved = np.dstack((r_convolved, g_convolved, b_convolved)).copy()
-    #pyplot.imshow("Original, Label, Convolved", np.hstack([rgba_original, label_mask, rgba_convolved]))
-    
-    pyplot.imshow(rgba_convolved)
-    pyplot.title("Convolved")
-    pyplot.figure()
-    pyplot.show()
+        #label_mask = np.ones((r_original.shape[0]-100,r_original.shape[1]-100))
+        label_mask = np.divide(label_mask, 15)
+        #print label_mask
+        list_mask = zip(np.where(label_mask<1)[1], np.where(label_mask<1)[0])
+        #pyplot.subplot(122),pyplot.imshow(label_mask, cmap="gray", interpolation="nearest")
+        #pyplot.title("label_mask");
+        
+        r_convolved = convolve(r_original, list_mask, convolution_filter)
+        g_convolved = convolve(g_original, list_mask, convolution_filter)
+        b_convolved = convolve(b_original, list_mask, convolution_filter)
+        rgba_convolved = np.dstack((r_convolved, g_convolved, b_convolved)).copy()
+        #pyplot.imshow("Original, Label, Convolved", np.hstack([rgba_original, label_mask, rgba_convolved]))
+        
+        pyplot.imshow(rgba_convolved)
+        pyplot.title("Convolved")
+        pyplot.figure()
+        pyplot.show()
 
 if __name__ == "__main__":
     main()
